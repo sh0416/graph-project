5b1eddee54b12378c342f6b8fbee6d96710bc83c
anthi7 <anthi7@naver.com>
2020-12-01 14:02:35 +0900

    Add files via upload

--- structural_distance.py
+++ structural_distance.py
@@ -121,81 +121,89 @@ def load():
     return edges, dic
 
 
-def get_neighborhood(adjacency, v):
-    return np.nonzero(adjacency[v].toarray()[0])[0]
+class StructuralDistance:
+    def __init__(self, adj, deg, k):
+        self.adjacency = adj
+        self.node_degree = deg
+        self.k = k
+    
+    
+    def get_neighborhood(self, v):
+        return np.nonzero(self.adjacency[v].toarray()[0])[0]
+    
+    
+    """
+    output: dict
+    output[n]: n hop neighborhood
+    """
+    def get_k_hop_neighborhood(self, v):
+        k_hop_neighborhood = {0: set([v])}
+        all_neighborhood = set([v])
+        for i in range(self.k):
+            prev_neighborhood = k_hop_neighborhood[i]
+            i_th_neighborhood = set()
+            for u in prev_neighborhood:
+                i_th_neighborhood = i_th_neighborhood.union(self.get_neighborhood(u).tolist())
+            k_hop_neighborhood[i+1] = i_th_neighborhood - all_neighborhood 
+            all_neighborhood = all_neighborhood.union(k_hop_neighborhood[i+1])
+            if len(k_hop_neighborhood[i+1]) == 0:
+                for j in range(i,self.k):
+                    k_hop_neighborhood[j+1] = set()
+                return k_hop_neighborhood
+        return k_hop_neighborhood
 
 
-"""
-output: dict
-output[n]: n hop neighborhood
-"""
-def get_k_hop_neighborhood(adjacency, v, k):
-    k_hop_neighborhood = {0: set([v])}
-    all_neighborhood = set([v])
-    for i in range(k):
-        prev_neighborhood = k_hop_neighborhood[i]
-        i_th_neighborhood = set()
-        for u in prev_neighborhood:
-            i_th_neighborhood = i_th_neighborhood.union(get_neighborhood(adjacency, u).tolist())
-        k_hop_neighborhood[i+1] = i_th_neighborhood - all_neighborhood 
-        all_neighborhood = all_neighborhood.union(k_hop_neighborhood[i+1])
-        if len(k_hop_neighborhood[i+1]) == 0:
-            for j in range(i,k):
-                k_hop_neighborhood[j+1] = set()
-            return k_hop_neighborhood
-    return k_hop_neighborhood
-
-
-def DTW(X,Y,
-        f_dist=lambda x,y:np.sqrt(np.sum((x-y)*(x-y)))
-       ):
-    #type check
-    if (type(X)!=np.ndarray or
-        type(Y)!=np.ndarray
-       ):
-        print('func_DTW: input type error')
-        return -1
-    #initialize cost matrix
-    l_X = len(X)
-    l_Y = len(Y)
-    cost = float('inf')*np.ones((l_X,l_Y))
-    #compute
-    cost[0,0]=f_dist(X[0],Y[0])
-    for i in range(1,l_X):
-        cost[i,0]=cost[i-1,0]+f_dist(X[i],Y[0])
-    for j in range(1,l_Y):
-        cost[0,j]=cost[0,j-1]+f_dist(X[0],Y[j])
-    for i in range(1,l_X):
+    def DTW(self,
+            X,Y,
+            f_dist=lambda x,y:np.sqrt(np.sum((x-y)*(x-y)))
+           ):
+        #type check
+        if (type(X)!=np.ndarray or
+            type(Y)!=np.ndarray
+           ):
+            print('func_DTW: input type error')
+            return -1
+        #initialize cost matrix
+        l_X = len(X)
+        l_Y = len(Y)
+        cost = float('inf')*np.ones((l_X,l_Y))
+        #compute
+        cost[0,0]=f_dist(X[0],Y[0])
+        for i in range(1,l_X):
+            cost[i,0]=cost[i-1,0]+f_dist(X[i],Y[0])
         for j in range(1,l_Y):
-            candidate = cost[i-1,j-1],cost[i,j-1],cost[i-1,j]
-            cost[i,j]=min(candidate)+f_dist(X[i],Y[j])
-    return cost[-1,-1]
+            cost[0,j]=cost[0,j-1]+f_dist(X[0],Y[j])
+        for i in range(1,l_X):
+            for j in range(1,l_Y):
+                candidate = cost[i-1,j-1],cost[i,j-1],cost[i-1,j]
+                cost[i,j]=min(candidate)+f_dist(X[i],Y[j])
+        return cost[-1,-1]
 
 
-def get_ordered_degree_seq(adjacency, node_degree, neighbors, memo = {}):
-    seq = []
-    for i in neighbors:
-        seq.append(node_degree[i])
-        #seq.append(get_degree(adjacency, i, memo = memo))
-    seq.sort()
-    return np.array(seq)
-
-
-def get_structural_distance(adjacency, node_degree, u, v, k, k_hop_u = None, k_hop_v = None):
-    if k_hop_u == None:
-        k_hop_u = get_k_hop_neighborhood(adjacency, u, k)
-    if k_hop_v == None:
-        k_hop_v = get_k_hop_neighborhood(adjacency, v, k)
+    def get_ordered_degree_seq(self, neighbors):
+        seq = []
+        for i in neighbors:
+            seq.append(self.node_degree[i])
+        seq.sort()
+        return np.array(seq)    
     
-    memo = {}
-    seq_u = get_ordered_degree_seq(adjacency, node_degree, k_hop_u[k], memo = memo)
-    seq_v = get_ordered_degree_seq(adjacency, node_degree, k_hop_v[k], memo = memo)
-    
-    dist = DTW(seq_u, seq_v)
-    
-    if k == 0:
-        return dist
-    return dist + get_structural_distance(adjacency, node_degree, u, v, k-1, k_hop_u, k_hop_v)   
+        
+    def __call__(self, u, v, k = None, k_hop_u = None, k_hop_v = None):
+        if k == None:
+            k = self.k
+        if k_hop_u == None:
+            k_hop_u = self.get_k_hop_neighborhood(u)
+        if k_hop_v == None:
+            k_hop_v = self.get_k_hop_neighborhood(v)
+
+        seq_u = self.get_ordered_degree_seq(k_hop_u[k])
+        seq_v = self.get_ordered_degree_seq(k_hop_v[k])
+
+        dist = self.DTW(seq_u, seq_v)
+
+        if k == 0:
+            return dist
+        return dist + self.__call__(u, v, k-1, k_hop_u, k_hop_v)   
 
 
 def main():
@@ -218,7 +226,8 @@ def main():
     adjacency = generate_adjacency_matrix(edges, num_node)
     node_degree = get_node_degree (edges)
     
-    dist = get_structural_distance(adjacency, node_degree, u, v, k)
+    dist_fn = StructuralDistance(adjacency, node_degree, k)
+    dist = dist_fn(u, v)
     print(dist)
 
 if __name__ == '__main__':
